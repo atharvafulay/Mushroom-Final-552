054c8c36d67b9084bf6acc71bfa2bc6e52b0e3ad
atharvafulay <atharva.fulay@gmail.com>
2020-05-10 00:28:47 -0700

    more changes, almost ready to submit

--- data_and_models.py
+++ data_and_models.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
 from sklearn.ensemble import RandomForestClassifier
@@ -17,32 +18,17 @@ def load_data():
             mushrooms.append(line.strip().split(','))
 
     df = pd.DataFrame(mushrooms)
-    # df.columns = cols # will have to do this manually
-
     df.columns = ['edible-posionous', 'cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 'gill-attachment',
                   'gill-spacing', 'gill-size', 'gill-color', 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring',
                   'stock-shape-below-ring', 'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-type', 'veil-color',
                   'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat']
 
-    df = df.apply(lambda x: pd.factorize(x)[0])
+    df = df.apply(lambda x: pd.factorize(x)[0])  # because factorized first, know that poisonous = 0, edible = 1
     df = shuffle(df)
     df.reset_index(inplace=True, drop=True)
 
     X = df.loc[:, df.columns != 'edible-posionous']
     y = df.loc[:, 'edible-posionous']
-
-    dirs = ['images', 'data', 'res']
-
-    for d in dirs:
-        try:
-            os.mkdir(d)
-        except FileExistsError:
-            pass
-
-    dat = ['agaricus-lepiota.data', 'agaricus-lepiota.names']
-    for d in dat:
-        if os.path.isfile(d):
-            os.rename(d, f'data/{d}')
 
     return X, y, list(df.columns)
 
@@ -62,6 +48,7 @@ def k_fold_model_analysis(X, y):
         X_train, X_test = X.loc[train_ind], X.loc[test_ind]
         y_train, y_test = y.loc[train_ind], y.loc[test_ind]
 
+        # using max_features='sqrt' for comparison purposes only
         rfc = RandomForestClassifier(n_jobs=-1, criterion='entropy', max_depth=md, max_features='sqrt')
         dtc = DecisionTreeClassifier(criterion='entropy', max_depth=md, max_features='sqrt')
 
@@ -91,6 +78,7 @@ def tradition_train_test(X, y):
     res_rfc = list()
     res_dtc = list()
 
+    # combinations of max_features, max_depth, class_weight
     for mf in [None, 'sqrt']:
         for md in range(1, 11):
             for cw in [None, 1]:
@@ -99,9 +87,9 @@ def tradition_train_test(X, y):
                     dtc = DecisionTreeClassifier(criterion='entropy', max_depth=md, max_features=mf)
                 else:
                     rfc = RandomForestClassifier(n_jobs=-1, criterion='entropy', max_depth=md, max_features=mf,
-                                                 class_weight={0: 1, 1: 100})
+                                                 class_weight={0: 100, 1: 1})
                     dtc = DecisionTreeClassifier(criterion='entropy', max_depth=md, max_features=mf,
-                                                 class_weight={0: 1, 1: 100})
+                                                 class_weight={0: 100, 1: 1})
 
                 rfc.fit(train_X, train_y)
                 rfc_train_score = rfc.score(train_X, train_y)
@@ -124,7 +112,8 @@ def tradition_train_test(X, y):
                 res_rfc.append(f'{rfc_train_score}, {rfc_score}, {md}, {mf_file}, {cw_file}\n')
                 res_dtc.append(f'{dtc_train_score}, {dtc_score}, {md}, {mf_file}, {cw_file}\n')
 
-    # separating these because it was causing me some issues when I had these within the triple loop above
+    # pulled out from loop because it was causing me some issues when I had these within the triple loop above
+    # rfc_results.txt and dtc_results.txt are used by analysis.py
     with open('res/rfc_results.txt', 'w') as f1:
         f1.write('#train, test, max_depth, max_features, class_weight\n')
 
@@ -145,7 +134,7 @@ def generate_optimal_tree(dtc, feat_names):
 
     tree.export_graphviz(dtc, 'optimal_tree.dot', feature_names=feat_names[1:])
     try:
-        call(['dolt', '-Tpng', 'optimal_tree.dolt', '-o', 'images/optimal_tree.png', '-Gdpi=60'])
+        call(['dot', '-Tpng', 'optimal_tree.dot', '-o', 'images/optimal_tree.png', '-Gdpi=60'])
         os.remove("optimal_tree.dot")
     except FileNotFoundError:
         print('Error with the .dot file. See below for details. The rest of the program will continue...')
@@ -158,13 +147,13 @@ def generate_optimal_tree(dtc, feat_names):
 
 
 def generate_feat_imp_visuals(X, y, columns):
+    print('\n---------- Generating Optimal RFC Feature Importances Visual ----------')
 
     train_X = X.loc[:5999]
     train_y = y.loc[:5999]
-    rfc = RandomForestClassifier(n_jobs=-1, criterion='entropy', class_weight={0: 1, 1: 100})
+    rfc = RandomForestClassifier(n_jobs=-1, criterion='entropy', class_weight={0: 100, 1:1})
     rfc.fit(train_X, train_y)
 
-    print('\n---------- Generating Optimal RFC Feature Importances Visual ----------')
     combined = zip(rfc.feature_importances_, columns[1:])
     combined = sorted(list(combined), reverse=True)
 
@@ -174,7 +163,6 @@ def generate_feat_imp_visuals(X, y, columns):
     for i in combined:
         sorted_feature_imps.append(i[0])
         sorted_cols.append(i[1])
-
 
     plt.bar(sorted_cols, sorted_feature_imps)
     plt.xticks(rotation=90)
@@ -188,57 +176,98 @@ def generate_feat_imp_visuals(X, y, columns):
 def generate_conf_matrix(X, y):
     print('\n---------- Confusion matrices for DTC and RFC ----------')
 
-    # note: the reason for using sqrt (for comparison purposes) is that with None, both models
-    # return identical results
+    # note: poisonous = 0, edible = 1
+    def to_words(lst):
+        res = list()
+        for item in lst:
+            if item == 1:
+                res.append('edible')
+            else:
+                res.append('poisonous')
+        return res
+
     train_X = X.loc[:5999]
     train_y = y.loc[:5999]
-    rfc = RandomForestClassifier(criterion='entropy', max_features='sqrt', class_weight={0: 1, 1: 100})
+
+    test_X = X.loc[6000:]
+    test_y = y.loc[6000:]
+
+    rfc = RandomForestClassifier(criterion='entropy', max_features=None, class_weight={0: 100, 1: 1})
     rfc.fit(train_X, train_y)
 
-    dtc = DecisionTreeClassifier(criterion='entropy', max_features='sqrt', class_weight={0: 1, 1: 100})
+    dtc = DecisionTreeClassifier(criterion='entropy', max_features=None, class_weight={0: 100, 1: 1})
     dtc.fit(train_X, train_y)
 
-    # note: poisonous = 0, edible = 1
-    predictions = rfc.predict(X)
-    print('RFC (max_features=sqrt, weighted):')
-    print(pd.crosstab(y, predictions, rownames=['Actual'], colnames=['Predicted']))
 
-    predictions = dtc.predict(X)
+    predictions = to_words(rfc.predict(X))  # entire dataset
+    actuals = to_words(list(y))
+    print('RFC (max_features=sqrt, weighted):')
+    print(pd.crosstab(np.array(actuals), np.array(predictions), rownames=['Actual'], colnames=['Predicted']))
+
+    predictions = to_words(dtc.predict(X))  # entire dataset
+    actuals = to_words(list(y))
     print('\nDTC (max_features=sqrt, weighted):')
-    print(pd.crosstab(y, predictions, rownames=['Actual'], colnames=['Predicted']))
+    print(pd.crosstab(np.array(actuals), np.array(predictions), rownames=['Actual'], colnames=['Predicted']))
 
     # same process as above but now with max-depth capped DTC and RFC
+    # note: the reason for using sqrt (for comparison purposes)
+    # with None, both models return identical results
     md = 5
-    rfc = RandomForestClassifier(criterion='entropy', max_features='sqrt', class_weight={0: 1, 1: 100}, max_depth=md)
+
+    # RFC
+    rfc = RandomForestClassifier(criterion='entropy', max_features='sqrt', class_weight={0: 100, 1: 1}, max_depth=md)
     rfc.fit(train_X, train_y)
 
-    dtc = DecisionTreeClassifier(criterion='entropy', max_features='sqrt', class_weight={0: 1, 1: 100}, max_depth=md)
+    predictions = to_words(rfc.predict(test_X))
+    actuals = to_words(list(test_y))
+    print('\nMax-depth capped RFC on test data (max_features=sqrt, weighted, max_depth=5):')
+    print(pd.crosstab(np.array(actuals), np.array(predictions), rownames=['Actual'], colnames=['Predicted']))
+
+    predictions = to_words(rfc.predict(X))
+    actuals = to_words(list(y))
+    print('\nMax-depth capped RFC on all data (max_features=sqrt, weighted, max_depth=5):')
+    print(pd.crosstab(np.array(actuals), np.array(predictions), rownames=['Actual'], colnames=['Predicted']))
+
+    # DTC
+    dtc = DecisionTreeClassifier(criterion='entropy', max_features='sqrt', class_weight={0: 100, 1: 1}, max_depth=md)
     dtc.fit(train_X, train_y)
 
-    predictions = rfc.predict(X)
-    print('\nMax-depth capped RFC (max_features=sqrt, weighted, max_depth=5):')
-    print(pd.crosstab(y, predictions, rownames=['Actual'], colnames=['Predicted']))
+    predictions = to_words(dtc.predict(test_X))
+    actuals = to_words(list(test_y))
+    print('\nMax-depth capped DTC on test data (max_features=sqrt, weighted, max_depth=5):')
+    print(pd.crosstab(np.array(actuals), np.array(predictions), rownames=['Actual'], colnames=['Predicted']))
 
-    predictions = dtc.predict(X)
-    print('\nMax-depth capped DTC (max_features=sqrt, weighted, max_depth=5):')
-    print(pd.crosstab(y, predictions, rownames=['Actual'], colnames=['Predicted']))
+    predictions = to_words(dtc.predict(X))
+    actuals = to_words(list(y))
+    print('\nMax-depth capped DTC on all data (max_features=sqrt, weighted, max_depth=5):')
+    print(pd.crosstab(np.array(actuals), np.array(predictions), rownames=['Actual'], colnames=['Predicted']))
 
 
 if __name__ == '__main__':
     plt.figure(figsize=(6, 8))
+    dirs = ['images', 'data', 'res']
+
+    for d in dirs:
+        try:
+            os.mkdir(d)
+        except FileExistsError:
+            pass
+
+    dat = ['agaricus-lepiota.data', 'agaricus-lepiota.names']
+    for d in dat:
+        if os.path.isfile(d):
+            os.rename(d, f'data/{d}')
 
     X, y, feat_names = load_data()
 
     # optimal random forest classifier
     train_X = X.loc[:5999]
     train_y = y.loc[:5999]
-    rfc = RandomForestClassifier(criterion='entropy', max_features=None, class_weight={0: 1, 1: 100})
+    rfc = RandomForestClassifier(criterion='entropy', max_features=None, class_weight={0: 100, 1: 1})
     rfc.fit(train_X, train_y)
 
     # optimal decision tree classifier
-    train_X = X.loc[:5999]
-    train_y = y.loc[:5999]
-    dtc = DecisionTreeClassifier(criterion='entropy', max_features=None, class_weight={0: 1, 1: 100})
+    dtc = DecisionTreeClassifier(criterion='entropy', max_features=None, class_weight={0: 100, 1: 1})
     dtc.fit(train_X, train_y)
 
     k_fold_model_analysis(X, y)
